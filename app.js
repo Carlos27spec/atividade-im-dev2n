f911f9b7a0b1a5a0d4265e486701d0dc10ad2b10
Carlos27Spec <kaduvieira27@gmail.com>
2026-08-12 21:03:58 -0300

    criação do template do perfll

---
+++ app.js
@@ -0,0 +1,35 @@
+const Perfil = document.getElementById("Perfil");
+const container = document.getElementById("container");
+
+Perfil.addEventListener("click", function () {
+  Perfil.classList.add("ativo");
+
+  container.innerHTML = `
+     <div class="foto">
+      <h1>Perfil</h1>
+      <img src="https://i.pravatar.cc/150?img=55" alt="" />
+    </div>
+    <div class="info">
+      <h2>Nome</h2>
+      <p>Ricardo da Silva</p>
+      <h2>Recado</h2>
+      <div class="sit">
+        <p>disponível</p>
+        <img src="./assets/icons/cursor-pointer.svg" alt="" />
+      </div>
+      <h2>Telefone</h2>
+      <div class="numero">
+        <img src="./assets/icons/phone.svg" alt="" />
+        <p>+55 11 98787 6567</p>
+        <img src="./assets/icons/copy-btn.svg" alt="" />
+      </div>
+    </div>
+    <div class="final">
+      <h2>Trocar de Perfil</h2>
+      <img src="https://i.pravatar.cc/150?img=55" alt="" />
+      <img src="https://i.pravatar.cc/150?img=55" alt="" />
+      <img src="https://i.pravatar.cc/150?img=55" alt="" />
+      <img src="https://i.pravatar.cc/150?img=55" alt="" />
+    </div>
+  `;
+});
