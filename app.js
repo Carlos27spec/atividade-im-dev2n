86df7dc9969239f9ca6ff8936ac87ea9a876bb34
Carlos27Spec <kaduvieira27@gmail.com>
2026-08-17 21:46:03 -0300

    função ajustando o html e puxando os usuários

--- app.js
+++ app.js
@@ -115,23 +115,27 @@ const Perfil = document.getElementById("Perfil");
 const container = document.getElementById("container");
 const contatosBase = container.innerHTML;
 
-Perfil.addEventListener("click", function () {
-  const telaAT = Perfil.classList.contains("ativo");
+function telaPerfil() {
+  const usuarioLogado = usuarios["whats-users"][userAT.usuarioAtual];
 
-  if (telaAT) {
-    Perfil.classList.remove("ativo");
-    container.innerHTML = contatos;
-  } else {
-    Perfil.classList.add("ativo");
+  const listaPerfis = usuarios["whats-users"]
+    .map(
+      (usuario, index) => `
+        <div class="perfil-item" data-index="${index}">
+          <img src="https://i.pravatar.cc/150?u=${usuario.number}" alt="" />
+          <p>${usuario.nickname}</p>
+        </div>
+      `,
+    )
+    .join("");
 
-    container.innerHTML = `
-      <h1 class="user">Perfil</h1>
+  return `<h1 class="user">Perfil</h1>
     <div class="foto">
-      <img src="https://i.pravatar.cc/150?img=55" alt="" />
+      <img src="https://i.pravatar.cc/150?u=${usuarioLogado.number}" alt="" />
     </div>
     <div class="info">
       <h2>Nome</h2>
-      <p>Ricardo da Silva</p>
+      <p>${usuarioLogado.account}</p>
       <h2>Recado</h2>
       <div class="sit">
         <p>disponível</p>
@@ -140,8 +144,8 @@ Perfil.addEventListener("click", function () {
       <h2>Telefone</h2>
       <div class="numero">
         <div class="numero-left">
-        <img src="./assets/icons/phone.svg" alt="" />
-        <p>+55 11 98787 6567</p>
+          <img src="./assets/icons/phone.svg" alt="" />
+          <p>${usuarioLogado.number}</p>
         </div>
         <img src="./assets/icons/copy-btn.svg" alt="" />
       </div>
@@ -149,25 +153,20 @@ Perfil.addEventListener("click", function () {
     <div class="final">
       <h2>Trocar de Perfil</h2>
       <div class="perfis">
-      <div class="perfil-item">
-      <img src="https://i.pravatar.cc/150?img=55" alt="" />
-      <p>Tom Cruise</p>
-      </div>
-      <div class="perfil-item">
-      <img src="https://i.pravatar.cc/150?img=55" alt="" />
-      <p>Giga</p>
-      </div>
-      <div class="perfil-item">
-      <img src="https://i.pravatar.cc/150?img=55" alt="" />
-      <p>Jaden</p>
-      </div>
-      <div class="perfil-item">
-      <img src="https://i.pravatar.cc/150?img=55" alt="" />
-      <p>Henrique</p>
-      </div>
+        ${listaPerfis}
       </div>
     </div>
   `;
+}
+
+Perfil.addEventListener("click", function () {
+  const telaAT = Perfil.classList.contains("ativo");
+
+  if (telaAT) {
+    voltarParaContatos();
+  } else {
+    Perfil.classList.add("ativo");
+    container.innerHTML = telaPerfil();
   }
 });
 
